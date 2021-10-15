cde8a86c511ac8d7ef0c4561a73f8d929b813fee
evolyuta <valproge@gmail.com>
2021-10-16 01:47:30 +0300

    fix users table fields and add user seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_manager')->default(false);
+
+            $table->unsignedBigInteger('manager_id')->nullable();
+            $table->foreign('manager_id')->references('id')->on('users');
+
             $table->rememberToken();
+
             $table->timestamps();
         });
     }
